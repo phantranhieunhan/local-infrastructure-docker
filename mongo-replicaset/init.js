739aa40de737c0d49ea12beeefbffb28dc55258f
Kien Dinh <kienmatu1@gmail.com>
2023-03-06 16:04:23 +0700

    Add mongodb replicaset

---
+++ mongo-replicaset/init.js
@@ -0,0 +1,10 @@
+// rs.initiate({
+//   _id: "rs0",
+//   members: [
+//     { _id: 0, host: "127.0.0.1:27017" },
+//     { _id: 1, host: "127.0.0.1:27016" },
+//     { _id: 2, host: "127.0.0.1:27015" },
+//   ],
+// });
+// open command after connectin and run this:
+rs.initiate();
